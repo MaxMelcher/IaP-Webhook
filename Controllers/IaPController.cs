48cf4abffe45a717906d63989c3628132fbf7dd6
Max Melcher <max@melcher.it>
2022-02-01 13:49:48 +0100

    switch, token decoded

--- Controllers/IaPController.cs
+++ Controllers/IaPController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace Apple_IaP_WebApi2
 {
@@ -15,9 +16,53 @@ namespace Apple_IaP_WebApi2
         }
 
         [HttpPost("WebHook")]
+        /// <summary>
+        /// WebHook triggered by Logic App
+        /// IaPEent example: https://gist.github.com/MaxMelcher/99562cfb4d906446a7364be63755b6c1
+        /// </summary>
         public ActionResult WebHook(IaPEvent iaPEvent)
         {
             _logger.LogInformation($"Received IaP event: {iaPEvent.notificationType}");
+
+            //decode signedTransactionInfo, see https://gist.github.com/MaxMelcher/99562cfb4d906446a7364be63755b6c1#file-signedtransactioninfo-json
+            var signedTransactionInfo = new JwtSecurityToken(iaPEvent.data.signedTransactionInfo);
+            //decode signedRenewalInfo, see https://gist.github.com/MaxMelcher/99562cfb4d906446a7364be63755b6c1#file-signedrenewalinfo-json
+            var signedRenewalInfo = new JwtSecurityToken(iaPEvent.data.signedRenewalInfo);
+
+            switch (iaPEvent.notificationType)
+            {
+                case "CONSUMPTION_REQUEST":
+                    break;
+                case "DID_CHANGE_RENEWAL_PREF":
+                    break;
+                case "DID_CHANGE_RENEWAL_STATUS":
+                    break;
+                case "DID_FAIL_TO_RENEW":
+                    break;
+                case "DID_RENEW":
+                    break;
+                case "EXPIRED":
+                    break;
+                case "GRACE_PERIOD_EXPIRED":
+                    break;
+                case "OFFER_REDEEMED":
+                    break;
+                case "PRICE_INCREASE":
+                    break;
+                case "REFUND":
+                    break;
+                case "REFUND_DECLINED":
+                    break;
+                case "RENEWAL_EXTENDED":
+                    break;
+                case "REVOKE":
+                    break;
+                case "SUBSCRIBED":
+                    break;
+                default:
+                    break;
+            }
+
             return Ok(iaPEvent);
         }
     }
--- Models/IaPEvent.cs
+++ Models/IaPEvent.cs
@@ -1,8 +1,34 @@
 namespace Apple_IaP_WebApi2
 {
+    /// <summary>
+    /// This class is sent by the Apple Server side events
+    /// see for values: https://gist.github.com/MaxMelcher/99562cfb4d906446a7364be63755b6c1
+    /// </summary>
     public class IaPEvent
     {
+        /// <summary>
+        /// Can be 
+        /// 'CONSUMPTION_REQUEST', 
+        /// 'DID_CHANGE_RENEWAL_PREF', 
+        /// 'DID_CHANGE_RENEWAL_STATUS',
+        /// 'DID_FAIL_TO_RENEW',
+        /// 'DID_RENEW',
+        /// 'EXPIRED', 
+        /// 'GRACE_PERIOD_EXPIRED', 
+        /// 'OFFER_REDEEMED', 
+        /// 'PRICE_INCREASE', 
+        /// 'REFUND', 
+        /// 'REFUND_DECLINED', 
+        /// 'RENEWAL_EXTENDED', 
+        /// 'REVOKE', 
+        /// 'SUBSCRIBED'
+        /// see https://developer.apple.com/documentation/appstoreservernotifications/notificationtype
+        /// </summary>
         public string notificationType { get; set; }
+
+        /// <summary>
+        /// Subtype, see: https://developer.apple.com/documentation/appstoreservernotifications/subtype
+        /// </summary>
         public string subtype { get; set; }
         public string notificationUUID { get; set; }
         public IaPData data { get; set; }
